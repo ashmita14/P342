ececc7aad8849f08028f1d544e931eadc8c8384d
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2020-08-06 19:54:22 +0530

    Add files via upload

---
+++ Question 1 (C++).cpp
@@ -0,0 +1,13 @@
+//Question 1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int i,n;
+int sum=0;
+cout << "Enter n"<< endl;
+cin>>n;
+for(i=1;i<=n;i++)  sum=sum+i;
+cout <<"Sum = " << sum << endl;
+}
