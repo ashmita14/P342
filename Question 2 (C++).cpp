15ac63ddf117b9fe47b8a1322099ce723d0a0b99
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2020-08-13 15:53:02 +0530

    Add files via upload

---
+++ Question 2 (C++).cpp
@@ -0,0 +1,34 @@
+//Question 2
+
+#include<iostream>
+using namespace std;
+
+int main(){
+//declaring vectors
+int A[3]={3,8,7};
+int B[3]={5,2,9};
+
+//declaring variables
+int S[3];
+int i, D=0;
+
+//calculating sum and dot product
+for(i=0;i<3;i++){
+S[i]=A[i]+B[i];  //A+B (sum)
+D=D+A[i]*B[i];   // A>B (dot product)
+}
+
+//printing results
+cout<<"Vector A : [" ;
+for(i=0;i<3;i++) cout<<A[i] << " ";
+cout<<"]"<<endl;
+cout<<"Vector B : [";
+for(i=0;i<3;i++) cout<<B[i] << " ";
+cout<<"]"<<endl;
+cout<<"Sum of A and B : [";
+for(i=0;i<3;i++) cout<<S[i] << " ";
+cout<<"]"<<endl;
+cout<<"Dot product of A and B = "<<D << endl;
+
+return 0;
+}
