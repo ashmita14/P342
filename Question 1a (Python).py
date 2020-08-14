6522b4426b046dcce9f3d74bd88a151ff35f99d5
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2020-08-14 17:08:43 +0530

    Update Question 1a (Python).py

--- Question 1a (Python).py
+++ Question 1a (Python).py
@@ -1,4 +1,4 @@
-#Question 1a
+#Question 1a   N=6 : o--o--o--o--o--o
 
 #Given number of points in  line
 N=6
@@ -17,3 +17,13 @@ print(f'Total number of points in a line = {N}')
 print(f'Total number of pairs possible = {totp}')
 print(f'Total Distance = {totdis}')
 print(f'Average Distance = {avgdis}')
+
+
+
+######### OUTPUT ######### (directly copy pasted from run window)
+#Total number of points in a line = 6
+#Total number of pairs possible = 36
+#Total Distance = 70
+#Average Distance = 1.9444444444444444
+#
+#Process finished with exit code 0
