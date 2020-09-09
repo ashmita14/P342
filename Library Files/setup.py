063ed46c3458644ed7edab63225927e41e9f1cbb
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2020-09-09 11:26:22 +0530

    Add files via upload

---
+++ Library Files/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mypythonlib',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='A PANDA',
+    author_email='ashmita.panda@niser.ac.in',
+    description=''
+)
