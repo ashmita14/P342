1a0b1de0f6f5c8df51dd6e8b566226b059e401d3
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2020-11-26 20:17:30 +0530

    Add files via upload

---
+++ Question 2/Question 2.py
@@ -0,0 +1,24 @@
+#Question 2
+
+import math
+import sys
+
+sys.path.append('C:/ASHMITA/NISER Study/5th Semester/P342 - Computational Lab/APanda_Lib')
+# importing all files at once, now we just need to write function name to access it
+from APanda_Lib import *
+
+#RK4
+f1=lambda dy,y,x:dy
+f2=lambda dy,y,x:1-x-dy
+x0=0
+y0=2
+dy0=1
+xmax=5
+xmin=-5
+for i in range(5):
+    h=float(input('Enter value of h for eqn.\n'))
+    N=int((xmax-xmin)/h)
+    n=f'x vs y for eqn, h={h} N={N}'
+    file=open(n, 'w') #refreshing file
+    RK4_double(f1,f2,x0,y0,dy0,h,N,n)
+    #
