6560c26acdf8826e5c6957856789961bbbeac51e
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2020-11-27 12:56:27 +0530

    Add files via upload

---
+++ Library Files/__init__.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('C:/ASHMITA/NISER Study/5th Semester/P342 - Computational Lab/APanda_Lib')
+
+from handling_files import *
+from lineareqns import *
+from non_lineareqns import *
+from numerical_integration import *
+from numerical_integration_2D import *
+from derivatives import *
+from T_int import *
+from arrays_operations import *
+from differential_equations import *
+
+
