0b238f3119b203475efe6f5053c8aeab3a094b30
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2021-01-29 16:22:33 +0530

    Update Question 1.py

--- Question 1/Question 1.py
+++ Question 1/Question 1.py
@@ -34,3 +34,46 @@ for j in range(5):
     append_file(nm1, f'{N} {Rrms}\n')
 
 #######################################
+
+
+
+
+######################### OUTPUT #################################
+#Enter value of N (no. of steps)
+#250
+#For N=250
+#RMS Value of Radial Distance (R) = 15.229181480111528
+#Average Value of displacement along x-axis = -2.1421040236083595
+#Average Value of displacement along y axis =-0.37145133370373196
+#
+#Enter value of N (no. of steps)
+#400
+#For N=400
+#RMS Value of Radial Distance (R) = 21.344343473468083
+#Average Value of displacement along x-axis = 0.43618998122651775
+#Average Value of displacement along y axis =0.8259966782170534
+#
+#Enter value of N (no. of steps)
+#500
+#For N=500
+#RMS Value of Radial Distance (R) = 21.86996340091884
+#Average Value of displacement along x-axis = -2.1346444774009874
+#Average Value of displacement along y axis =-2.735873831597171
+#
+#Enter value of N (no. of steps)
+#750
+#For N=750
+#RMS Value of Radial Distance (R) = 25.667787649772517
+#Average Value of displacement along x-axis = -1.0307460071647516
+#Average Value of displacement along y axis =0.9538076454593591
+#
+#Enter value of N (no. of steps)
+#1000
+#For N=1000
+#RMS Value of Radial Distance (R) = 31.042902564091282
+#Average Value of displacement along x-axis = -2.3153947887926063
+#Average Value of displacement along y axis =-0.4602628273242385
+#
+#
+#Process finished with exit code 0
+
