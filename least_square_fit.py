fd888fc643bcff94921a9042aaa11b0693a1bb1b
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2021-02-04 11:39:24 +0530

    Add files via upload

--- least_square_fit.py
+++ least_square_fit.py
@@ -3,20 +3,21 @@ import math
 import lineareqns
 import handling_files
 
-def least_square(M,n,nm):
-    print(n)
+def least_square(M,nm):
+    n=len(M)
     x1=0
     x2=0
     y1=0
     x1y1=0
     for i in range(n):
-        x1=x1+M[0][i]
-        x2=x2+math.pow(M[0][i],2)
-        y1=y1+M[1][i]
-        x1y1=xiyi+(M[0][i]*M[1][j])
+        x1=x1+M[i][0]
+        x2=x2+math.pow(M[i][0],2)
+        y1=y1+M[i][1]
+        x1y1=x1y1+(M[i][0]*M[i][1])
         #
-    handling_files.append_file(nm, f'{n} {x1}\n{x1} {x2}')
-    Y=[0 0]
+    handling_files.append_file(nm, f'{n} {x1}\n')
+    handling_files.append_file(nm,f'{x1} {x2}\n')
+    Y=[0,0]
     Y[0]=y1
     Y[1]=x1y1
     A1=handling_files.read_matrix(nm)
@@ -24,4 +25,24 @@ def least_square(M,n,nm):
     Coef=lineareqns.forward_backward(A,Y)
     return(Coef)
 
-
+def pearson(M):
+    n=len(M)
+    x1=0
+    y1=0
+    x1y1=0
+    for i in range(n):
+        x1 = x1 + M[i][0]
+        y1 = y1 + M[i][1]
+        #
+    xavg=x1/n
+    yavg=y1/n
+    sxx=0
+    syy=0
+    sxy=0
+    for i in range(n):
+        sxx=sxx+math.pow((M[i][0]-xavg),2)
+        syy=syy+math.pow((M[i][1]-yavg),2)
+        sxy=sxy+(M[i][0]-xavg)*(M[i][1]-yavg)
+        #
+    r2=sxy*sxy/(sxx*syy)
+    return(r2)
--- q3.py
+++ q3.py
@@ -9,7 +9,13 @@ from APanda_Lib import *
 
 #reading matrix from file
 M=read_matrix('q3data.txt')
-name='q3matrix elements store.txt'
+
+#first fitting
+name='q3(i)matrix elements store.txt'
 file=open(name,'w')
 C=least_square(M,name)
-print(C)
+print('For First Fitting : \n')
+print(f'Fit Function for (i) : \nw(t)={C[0]}+({C[1]})t')
+#finding Pearson's r for first fitting
+R2_i=pearson(M)
+print(f'Value of Pearson`s r :\nr^2={R2_i}\nr={math.sqrt(R2_i)}\n')
