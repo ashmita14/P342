e5da2e0c4afe562d5d6a97bcaf7090c26732403c
ashmita14 <42172528+ashmita14@users.noreply.github.com>
2021-02-04 10:30:08 +0530

    Add files via upload

--- q1.py
+++ q1.py
@@ -8,18 +8,18 @@ sys.path.append('C:/ASHMITA/NISER Study/5th Semester/P342 - Computational Lab/AP
 from APanda_Lib import *
 
 
-f(x)= lambda x: (x-5)*exp(x)+5
+f= lambda x: (x-5)*math.exp(x)+5
 nm='Absolute Errors.txt'
 file=open(nm, 'w') #cleaning file
 max=100 #setting max iterations to 100
-a=int(input('Enter guess value of root'))
+a=int(input('Enter guess value of root.\n'))
 
 h=6.626*math.pow(10,-34)
 c=3*math.pow(10,8)
 k=1.381*math.pow(10,-23)
 
-root, check=newton_rhapson(f, a,max,nm)
-if check==true:
+root, check=newton_raphson(f, a,max,nm)
+if check==True:
     print(f'Root value of x obtained at {root}.')
     b=(h*c)/(root*k)
     print(f'Value of Wein`s constant obtained = {b}')
